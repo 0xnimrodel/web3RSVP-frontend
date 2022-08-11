940d02068284c612cb99b77356f631ee4814817d
zepgursoy <34766972+zepgursoy@users.noreply.github.com>
2022-08-11 22:35:27 +0300

    updating ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,4 +16,5 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
   ],
+  darkMode: 'class',
 };
