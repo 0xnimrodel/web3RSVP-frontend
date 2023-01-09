2431e0de3cbd1bd7fcd1126188feb83dd693d019
0xnimrodel <34766972+0xnimrodel@users.noreply.github.com>
2023-01-09 16:19:21 +0300

    Update apollo-client.js

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/zepgursoy/web3-rsvp',
+  uri: 'https://api.thegraph.com/subgraphs/name/0xnimrodel/web3-rsvp',
   cache: new InMemoryCache()
 })
 
